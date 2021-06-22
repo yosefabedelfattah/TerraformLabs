1713b16d14cf5feaea3add9966036b5a7af809db
yosefabedelfattah <yosuf.abed@gmail.com>
2021-06-22 14:09:38 +0300

    update 3

--- Lab2/azurevnet.tf
+++ Lab2/azurevnet.tf
@@ -4,7 +4,7 @@ resource "azurerm_virtual_network" "example" {
   location            = var.location
   resource_group_name = var.resourcegrpName
   address_space       = ["10.0.0.0/24"]
-  tags  = { var.tags }
+  tags  = var.tags
 }
 
 resource "azurerm_subnet" "mySubnet1" {
@@ -12,7 +12,7 @@ resource "azurerm_subnet" "mySubnet1" {
   resource_group_name  = var.resourcegrpName
   virtual_network_name = var.vnetName
   address_prefixes     = ["10.0.0.0/25"]
-  tags  = { var.tags }
+  tags  = var.tags
 }
 
 resource "azurerm_subnet" "mySubnet2" {
@@ -20,5 +20,5 @@ resource "azurerm_subnet" "mySubnet2" {
   resource_group_name  = var.resourcegrpName
   virtual_network_name = var.vnetName
   address_prefixes     = ["10.0.0.128/25"]
-  tags  = { var.tags }
+  tags  = var.tags
 }
